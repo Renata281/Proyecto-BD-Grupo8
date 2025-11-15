fec4f71f2a16b711c43256cee3b38aa7f3ab38fc
Yolanda-arias19 <yolanda2002.ya@gmail.com>
2025-11-15 17:16:56 -0300

    Update transaccion_consistente.sql

--- Manejo de transacciones y transacciones anidadas/transaccion_consistente.sql
+++ Manejo de transacciones y transacciones anidadas/transaccion_consistente.sql
@@ -1,23 +1,23 @@
 Utilizar hoteleria;
 
-/** 1-insertar registros en diferentes tablas y actualizar**/
+/** 1-insertar registros en tabla cliente y reserva. Luego actualizar estado.**/
 BEGIN TRY
     BEGIN TRANSACTION;
 
-    /Insertar un nuevo cliente/
+    /**Insertar un nuevo cliente**/
     INSERT INTO cliente (nombre, apellido, email, telefono, dni)
     VALUES ('Ana', 'Gómez', 'ana.gomez@example.com', 1122334455, 44556677);
 
     DECLARE @nuevoCliente INT = SCOPE_IDENTITY();
 
-    /Insertar una reserva/
+    /**Insertar una reserva**/
     INSERT INTO reserva (fecha_ingreso, fecha_salida, monto_total, id_cliente, nro_habitacion, id_piso, id_pago)
     VALUES ('2025-11-10', '2025-11-15', 150000, @nuevoCliente, 101, 1, 1);
 
-    /*Actualizar estado de la habitación (por ejemplo, marcarla como ocupada), 
-    La actualización sólo debe realizarse si toda la transacción se completa correctamente.*/
+    /**Actualizar estado de la habitación (por ejemplo, marcarla como ocupada), 
+    La actualización sólo debe realizarse si toda la transacción se completa correctamente.**/
     UPDATE habitacion
-    SET id_estado = 2  -- 2 = Ocupada (por ejemplo)
+    SET id_estado = 2  -- 2 = Ocupada 
     WHERE nro_habitacion = 101 AND id_piso = 1;
 
 
@@ -32,21 +32,21 @@ BEGIN CATCH
 END CATCH;
 
 
-/* 2- Provocar un error intencionalmente*/
+/** 2- Provocar un error intencionalmente**/
 BEGIN TRY
     BEGIN TRANSACTION;
 
-    /Insertar cliente/
+    /**Insertar cliente**/
     INSERT INTO cliente (nombre, apellido, email, telefono, dni)
     VALUES ('Carlos', 'Pérez', 'carlos.perez@example.com', 1166778899, 99887766);
 
     DECLARE @nuevoCliente INT = SCOPE_IDENTITY();
 
-    /* Error intencional: insertar reserva con ID de habitación inexistente*/
+    /**Error intencional: insertar reserva con ID de habitación inexistente**/
     INSERT INTO reserva (fecha_ingreso, fecha_salida, monto_total, id_cliente, nro_habitacion, id_piso, id_pago)
     VALUES ('2025-11-20', '2025-11-25', 200000, @nuevoCliente, 999, 9, 1);  /no existe esta habitación/piso/
 
-   /Actualización (no debería llegar a ejecutarse)/
+   /**Actualización (no debería llegar a ejecutarse)**/
     UPDATE habitacion
     SET id_estado = 2
     WHERE nro_habitacion = 999 AND id_piso = 9;
