c503c610703e6a6e16133890d5f37eea6c5b1bdf
Yolanda-arias19 <yolanda2002.ya@gmail.com>
2025-11-16 19:38:33 -0300

    Update transaccion_consistente.sql

--- Manejo de transacciones y transacciones anidadas/transaccion_consistente.sql
+++ Manejo de transacciones y transacciones anidadas/transaccion_consistente.sql
@@ -160,8 +160,3 @@ TRY…CATCH permitió manejar el error sin que la base quede en un estado incons
 SQL Server solo confirma realmente cuando se hace el COMMIT final de la transacción externa.
 
 
-
-
-
-
-
